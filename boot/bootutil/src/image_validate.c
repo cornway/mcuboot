8136bdf7cb9f64e9109144e490b94db28c003de3
Roman Pustobaiev <romanpustobayev@gmail.com>
2025-11-24 22:07:15 +0100

    Adding grinreflex board Skip image validation until further fix

--- boot/bootutil/src/image_validate.c
+++ boot/bootutil/src/image_validate.c
@@ -198,6 +198,7 @@ bootutil_img_validate(struct boot_loader_state *state,
                       int seed_len, uint8_t *out_hash
                      )
 {
+    FIH_RET(FIH_SUCCESS);
 #if (defined(EXPECTED_KEY_TLV) && defined(MCUBOOT_HW_KEY)) || defined(MCUBOOT_HW_ROLLBACK_PROT)
     int image_index = (state == NULL ? 0 : BOOT_CURR_IMG(state));
 #endif
